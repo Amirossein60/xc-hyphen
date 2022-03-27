2b30f4713c954ff7ee2419c189463b1bd338898f
Amir <amir@gmail.com>
2022-03-27 17:26:13 +1100

    Initial Commit

---
+++ Source control/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Source control
+//
+//  Created by Amir Hossein on 27/3/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Source control/Source_controlApp.swift
@@ -0,0 +1,17 @@
+//
+//  Source_controlApp.swift
+//  Source control
+//
+//  Created by Amir Hossein on 27/3/2022.
+//
+
+import SwiftUI
+
+@main
+struct Source_controlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
